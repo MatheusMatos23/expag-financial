0726a17cde4e62aefe010daedfcc0488e214de20
Matheus Matos <matheusmatos.eng@hotmail.com>
2026-05-12 01:48:37 -0300

    fix: migracao com breakpoints para MySQL

--- drizzle/0001_add_created_by_name.sql
+++ drizzle/0001_add_created_by_name.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `revenues` ADD COLUMN `createdByName` varchar(200);
+--> statement-breakpoint
 ALTER TABLE `expenses` ADD COLUMN `createdByName` varchar(200);
+--> statement-breakpoint
 ALTER TABLE `payables` ADD COLUMN `createdByName` varchar(200);
